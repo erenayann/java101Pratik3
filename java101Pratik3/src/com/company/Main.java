3a0747ed2476567bae85e22dbfcca050f29e8766
erenayann <eren.ayan147@gmail.com>
2023-09-15 16:08:54 +0300

    Patika.dev Java101 Pratik 3

---
+++ java101Pratik3/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int a,b,c,u;
+        double alan;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("1.Kenarı Giriniz :");
+        a= input.nextInt();
+        System.out.print("2.Kenarı Giriniz :");
+        b= input.nextInt();
+        System.out.print("3.Kenarı Giriniz :");
+        c= input.nextInt();
+
+        u = (a+b+c) / 2;
+
+        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println("Üçgenin alanı = " +alan);
+    }
+}
